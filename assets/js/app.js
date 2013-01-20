4565f02692c767ad2380f616b38847ab01d13616
pohyz <pohyz@hotmail.com>
2013-01-20 18:17:35 +0800

    stupid mistake

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@
 angular.module('osmosis', ['ngResource']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/login', {templateUrl: 'login.html',   controller: UserCtrl}).
+      when('/login', {templateUrl: 'login.html',   controller: LoginCtrl}).
       when('/user_type', {templateUrl: 'user_type.html', controller: UserCtrl}).
       when('/home', {templateUrl: 'home.html', controller: ProfileCtrl}).
       when('/profile', {templateUrl: 'profile.html', controller: ProfileCtrl}).
