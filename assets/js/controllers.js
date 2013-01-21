e636e319a4589ecb6f81180c154d463110ec9939
pohyz <pohyz@hotmail.com>
2013-01-21 17:47:40 +0800

    try scope

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -29,7 +29,8 @@ function UserTypeCtrl($scope, $resource) {
   $scope.haha = "abc";
   $scope.updateUserType_C = function(){
 
-    $scope.Model.send({'user_type':$scope.user_type_i,'ut_method':'set_user_type_c'},function(response){$scope.haha=response.redirect_url;});
+    //$scope.Model.send({'user_type':$scope.user_type_i,'ut_method':'set_user_type_c'},function(response){$scope.haha=response.redirect_url;});
+    $scope.haha = $scope.user_type_c;
   };
 
   $scope.updateUserType_I = function(){
